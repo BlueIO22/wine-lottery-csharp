3012a5d23c5fa91997f2a552db34e8b25e4c30c6
BlueIO22 <marius.sorenes@catenda.no>
2023-04-24 08:25:52 +0200

    added test endpoint

--- Controllers/LotteryController.cs
+++ Controllers/LotteryController.cs
@@ -70,6 +70,12 @@ namespace wine_lottery_csharp.Controllers
 
             return Ok(result);
         }
+        
+        [HttpGet("get-feedback")]
+        public Task<ActionResult> GetFeedback(string lmao) 
+        {
+            return Ok(lmao);    
+        }
     }
 
 
